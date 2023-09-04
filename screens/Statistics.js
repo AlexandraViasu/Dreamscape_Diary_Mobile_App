b431d3ae7f40feba823dc872e7682c9663cc34ca
Quartermaster-90 <christopher.belsey@icloud.com>
2023-09-04 15:37:28 +0100

    icons added to menu bar

---
+++ screens/Statistics.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react'
+
+export default function Home() {
+    return (
+        <View style={{felx: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>Statistics</Text>
+        </View>
+    )
+}
---
+++ screens/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import AddADream from "./AddADream";
+import DreamJournal from "./DreamJournal";
+import Statistics from "./Statistics";
+
+export {
+    Home,
+    AddADream,
+    DreamJournal,
+    Statistics
+}
